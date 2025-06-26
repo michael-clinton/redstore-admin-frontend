742eaa986537e2d78f0b1787ab2ce38d3704e02e
michael-clinton <michaelclinton084@gmail.com>
2025-06-26 14:05:27 +0530

    fix: resolved build failure by correcting axiosInstance import path in Typography.jsx

--- src/pages/component-overview/Typography.jsx
+++ src/pages/component-overview/Typography.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import axiosInstance from "../../api/axiosInstance";
+import axiosInstance from "../../api/axios";
 import slugify from "slugify";
 import {
   Box,
